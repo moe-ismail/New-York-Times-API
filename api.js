6e4af01896d71ee97e52547267adfd573728530a
moe-ismail <moeismail.10@gmail.com>
2017-10-19 12:18:00 -0400

    fixes

--- api.js
+++ api.js
@@ -1,41 +1,70 @@
-$(document).ready(function  (){
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+
+
+$("#selectDropdown") .on("change", function(){
+$('#newsblock').empty();
+var val = $("#selectDropdown option:selected").val();
+var url ="https://api.nytimes.com/svc/topstories/v2/"+ val +".json";
+
 url += '?' + $.param({
-  'api-key': "65a9dda2f2414b3d966a836b6c01c94b"
+'api-key': "b9c05d24b71e49bf9b72e24feaff8b0b"
 });
 
- $.ajax({
-  url: url,
-  type: 'GET',
-}).done(function(url) {
-  console.log(url);
+$.ajax({
+url: url,
+method: 'GET',
+}).done(function(result) {
+
+            var count = 1;
+
+for (var i=0; i<result.results.length; i++) {
+
+
+          if (count <=12){
+
+
+if (result.results[i].multimedia.length>0){
+
+    var abstract = '<p class = "abstract">'+result.results[i].abstract +' </p> ' ;
+    var link = result.results[i].url;
+    var image = result.results[i].multimedia[4].url;
+    var htmlElements =  '<a href="' +link +'">' +abstract + ' </a> ';
+
+    var createDiv = $("<div></div>");
+    createDiv.addClass('singlenews');
+    $('#newsblock').append(createDiv);
+    createDiv.css('background-image', "url(" + image + ")");
+    createDiv.append(htmlElements);
+    $('.logoAndDropdown').addClass('minified');
+
+
+
+  console.log (count++);
+}
+} else{
+break;
+}
+}
+
 }).fail(function(err) {
-  throw err;
-});
-});
-
-$(document).ready(function(){
-    $("#hide").click(function(){
-        $(".loader").hide();
-    });
-    $("#show").click(function(){
-        $(".loader").show();
-    });
+throw err;
 });
 
-jQuery('<div/>', {
-  class:'pictures2',
-  href: 'https://static01.nyt.com/images/2017/07/26/world/25USBriefing-Kushner/25USBriefing-Kushner-superJumbo.jpg',
-  title:'Kushner Briefing',
-  rel:'external',
-  text:''
-}).appendTo('.pictures2');
+});
 
-jQuery('<div/>',{
-  class: 'text2',
-  href:"https://www.nytimes.com/2017/07/25/us/politics/right-and-left-react-to-kushner-sessions-and-more.html",
- title:"Right and Left React to Kushner, Sessions and More",
- rel:'external',
- text:'Read about how the other side thinks: Writers from across the political spectrum react to the news on the Russian meddling investigation and more.'
-
-}).appendTo('.pictures3');
